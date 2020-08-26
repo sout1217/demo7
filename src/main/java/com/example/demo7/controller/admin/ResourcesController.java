8d8b70428e1808400c81ed2ff03c24be1673f9f1
DESKTOP-TRJT0MI\Admin <sout1217@gmail.com>
2020-08-26 23:05:56 +0900

    웹 기반 인가처리 실시간 반영하기

--- src/main/java/com/example/demo7/controller/admin/ResourcesController.java
+++ src/main/java/com/example/demo7/controller/admin/ResourcesController.java
@@ -4,6 +4,7 @@ import com.example.demo7.domain.dto.ResourcesDto;
 import com.example.demo7.domain.entity.Resources;
 import com.example.demo7.domain.entity.Role;
 import com.example.demo7.repository.RoleRepository;
+import com.example.demo7.security.metadatasource.UrlFilterInvocationSecurityMetadataSource;
 import com.example.demo7.service.ResourcesService;
 import com.example.demo7.service.RoleService;
 import org.modelmapper.ModelMapper;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashSet;
 import java.util.List;
@@ -25,6 +27,27 @@ public class ResourcesController {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private UrlFilterInvocationSecurityMetadataSource filterInvocationSecurityMetadataSource;
+
+    @PostMapping("/admin/resources")
+    public String createResources(ResourcesDto resourcesDto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Role role = roleRepository.findByRoleName(resourcesDto.getRoleName());
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        Resources resources = modelMapper.map(resourcesDto, Resources.class);
+        resources.setRoleSet(roles);
+
+        resourcesService.createResources(resources);
+        filterInvocationSecurityMetadataSource.reload();
+
+        return "redirect:/admin/resources";
+    }
 
     @GetMapping("/admin/resources")
     public String getResources(Model model) {
@@ -64,5 +87,13 @@ public class ResourcesController {
         model.addAttribute("resources", resourcesDto);
 
         return "admin/resource/detail";
+    }
+
+    @GetMapping("/admin/resources/delete/{id}")
+    public String removeResources(@PathVariable String id, Model model) {
+//        Resources resources = resourcesService.getResources(Long.valueOf(id));
+        resourcesService.deleteResources(Long.valueOf(id));
+        filterInvocationSecurityMetadataSource.reload();
+        return "redirect:/admin/resources";
     }
 }
--- src/main/java/com/example/demo7/security/metadatasource/UrlFilterInvocationSecurityMetadataSource.java
+++ src/main/java/com/example/demo7/security/metadatasource/UrlFilterInvocationSecurityMetadataSource.java
@@ -1,16 +1,23 @@
 package com.example.demo7.security.metadatasource;
 
+import com.example.demo7.service.SecurityResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
+@Component
 public class UrlFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
 
     private LinkedHashMap<RequestMatcher, List<ConfigAttribute>> requestMap = new LinkedHashMap<>();
+
+    @Autowired
+    private SecurityResourceService securityResourceService;
 
     public UrlFilterInvocationSecurityMetadataSource(LinkedHashMap<RequestMatcher, List<ConfigAttribute>> resourceMap) {
         this.requestMap = resourceMap;
@@ -48,5 +55,18 @@ public class UrlFilterInvocationSecurityMetadataSource implements FilterInvocati
     @Override
     public boolean supports(Class<?> clazz) {
         return FilterInvocation.class.isAssignableFrom(clazz);
+    }
+
+    public void reload() {
+
+        LinkedHashMap<RequestMatcher, List<ConfigAttribute>> reloadMap = securityResourceService.getResourceList();
+        Iterator<Map.Entry<RequestMatcher, List<ConfigAttribute>>> iterator = reloadMap.entrySet().iterator();
+
+        requestMap.clear();
+
+        while (iterator.hasNext()) {
+            Map.Entry<RequestMatcher, List<ConfigAttribute>> entry = iterator.next();
+            requestMap.put(entry.getKey(),entry.getValue());
+        }
     }
 }
